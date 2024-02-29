c76519118bd865188c31c138e6e65e37f0c7a65f
aditya-840 <107612559+aditya-840@users.noreply.github.com>
2024-02-29 20:23:37 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,30 @@
+let screen = document.getElementById('inputBox');
+buttons = document.querySelectorAll('.button');
+let screenValue = '';
+for (item of buttons) {
+    item.addEventListener('click', (e) => {
+        buttonText = e.target.innerText;
+        console.log('Button text is ', buttonText);
+        if (buttonText == '*') {
+            buttonText = '*';
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'AC') {
+            screenValue = "";
+            screen.value = screenValue;
+        }
+        else if (buttonText == 'DEL') {
+            screenValue = screenValue.slice(0, -1); // Remove the last character
+            screen.value = screenValue;
+        }
+        else if (buttonText == '=') {
+            screen.value = eval(screenValue);
+        }
+        else {
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+
+    })
+}
